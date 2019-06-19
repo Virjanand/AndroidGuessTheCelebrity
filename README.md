cc7cca68323bb2108563b93925aa78d912162478
Virjanand <virjanand@virjanand.com>
2019-06-19 06:06:40 +0200

    Display one downloaded image in top half of screen

--- README.md
+++ README.md
@@ -4,10 +4,27 @@ is displayed and the user has to guess the name of the celebrity out of four
 randomly selected names from the same website.
 
 # ToDo
-- [ ] Display one image in top half of screen
+- [x] Display one image in top half of screen
 - [ ] Display 4 buttons in bottom half of screen
 - [ ] Display name in button in bottom half of screen
 - [ ] Display 3 random names in other buttons
 - [ ] Suffle buttons
 - [ ] When correct button is pressed display toast with correct
 - [ ] When incorrect button is pressed: Wrong! It was ...
+
+ String html= "<div class=\"image\"><img src=\"http://cdn.posh24.se/images/:profile/088110b3627723dd6f73b718905c2498f\" alt=\"Lady Gaga\"/></div>";
+    
+    Pattern p = Pattern.compile("src=\\\"(.*?)\"");
+    Matcher m = p.matcher(html);
+    
+    while(m.find()) {
+    	System.out.println(m.group(1));
+    }
+    
+    p = Pattern.compile("alt=\\\"(.*?)\"");
+    m = p.matcher(html);
+    
+    while(m.find()) {
+    	System.out.println(m.group(1));
+    }
+  }
--- app/src/main/java/com/virjanand/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/virjanand/guessthecelebrity/MainActivity.java
@@ -1,7 +1,17 @@
 package com.virjanand.guessthecelebrity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        try {
+            imageView.setImageBitmap(new ImageDownloader().execute("http://cdn.posh24.se/images/:profile/088110b3627723dd6f73b718905c2498f/").get());
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream in = connection.getInputStream();
+                return BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
 }
